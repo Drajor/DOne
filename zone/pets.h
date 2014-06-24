c19446f25077fce1624c70977718fa76a03074c9
Drajor <DrajorEQ@gmail.com>
2014-06-24 18:14:59 +1000

    Minor tidy in Pets

--- zone/pets.h
+++ zone/pets.h
@@ -1,26 +1,46 @@
+/*	EQEMu: Everquest Server Emulator
+Copyright (C) 2001-2002 EQEMu Development Team (http://eqemu.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY except by those people which sell it, which
+are required to give you total support for your newly bought product;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
 #ifndef PETS_H
 #define PETS_H
 
-	#define PET_BACKOFF			1
-	#define PET_GETLOST			2
-	#define PET_HEALTHREPORT	4
-	#define PET_GUARDHERE		5
-	#define PET_GUARDME			6
-	#define PET_ATTACK			7
-	#define PET_FOLLOWME		8
-	#define PET_SITDOWN			9
-	#define PET_STANDUP			10
-	#define PET_TAUNT			11
-	#define PET_HOLD			12
-	#define PET_NOTAUNT			14
-	#define PET_LEADER			16
-	#define	PET_SLUMBER			17
-	#define	PET_NOCAST			18
-	#define	PET_FOCUS			19
-	#define	PET_FOCUS_ON		25
-	#define	PET_FOCUS_OFF		26
-	#define PET_HOLD_ON			27
-	#define PET_HOLD_OFF		28
+#include "../common/types.h"
+
+static const uint32 PET_BACKOFF = 1;
+static const uint32 PET_GETLOST = 2;
+static const uint32 PET_HEALTHREPORT = 4;
+static const uint32 PET_GUARDHERE = 5;
+static const uint32 PET_GUARDME = 6;
+static const uint32 PET_ATTACK = 7;
+static const uint32 PET_FOLLOWME = 8;
+static const uint32 PET_SITDOWN = 9;
+static const uint32 PET_STANDUP = 10;
+static const uint32 PET_TAUNT = 11;
+static const uint32 PET_HOLD = 12;
+static const uint32 PET_NOTAUNT = 14;
+static const uint32 PET_LEADER = 16;
+static const uint32 PET_SLUMBER = 17;
+static const uint32 PET_NOCAST = 18;
+static const uint32 PET_FOCUS = 19;
+static const uint32 PET_FOCUS_ON = 25;
+static const uint32 PET_FOCUS_OFF = 26;
+static const uint32 PET_HOLD_ON = 27;
+static const uint32 PET_HOLD_OFF = 28;
 
 	class Pet : public NPC {
 	public:
