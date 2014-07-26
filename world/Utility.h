cbbdd6b5f5472aa4ec7db0022786db71ba720e77
Drajor <DrajorEQ@gmail.com>
2014-07-27 08:49:17 +1000

    Missing from last commit

--- world/Utility.h
+++ world/Utility.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <sstream>
+#include "LogSystem.h"
 
 namespace Utility {
 	void print(std::string pMessage);
@@ -12,4 +13,47 @@ namespace Utility {
 		return std::string(pCString);
 	}
 
+	class DynamicStructure {
+	public:
+		template <typename T>
+		DynamicStructure(T pStart, int pSize) : mStart(reinterpret_cast<char*>(pStart)), mPointer(mStart), mSize(pSize), mWritten(0) {}
+
+		template <typename T>
+		void write(T pData) {
+			if (mWritten + sizeof(T) <= mSize) {
+				auto TPtr = reinterpret_cast<T*>(mPointer);
+				*TPtr = pData;
+				mPointer += sizeof(T);
+				mWritten += sizeof(T);
+			}
+			else {
+				Log::error("[Dynamic Structure] Attempt to write to invalid memory.");
+			}
+		}
+
+		void writeString(std::string pData) {
+			if (mWritten + pData.length() + 1 <= mSize) {
+				strcpy(mPointer, pData.c_str());
+				mPointer += pData.length() + 1;
+				mWritten += pData.length() + 1;
+			}
+			else {
+				Log::error("[Dynamic Structure] Attempt to write to invalid memory.");
+			}
+		}
+
+		void movePointer(int pBytes) {
+			// TODO: Check this!
+			mPointer += pBytes;
+			mWritten += pBytes; // I need to think about this more.
+		}
+		int getSize() { return mSize; }
+		int getBytesWritten() { return mWritten; }
+
+	private:
+		int mSize; // maximum number of bytes to be written.
+		int mWritten; // number of bytes written.
+		char* mStart; // start memory location.
+		char* mPointer; // current location in memory.
+	};
 }
